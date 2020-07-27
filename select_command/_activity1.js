f9b2c6405ebc11c2226e0963fc20ab7b354d700f
kimtienvu <kimtienvu01@gmail.com>
2020-07-27 11:21:35 -0700

    Add _index.md,_basics.md, rawhtml shortcode, updated activity1

---
+++ select_command/_activity1.js
@@ -0,0 +1,70 @@
+//Global variables to keep track of answer places
+var p1 = null;
+var p2 = null;
+var p3 = null;
+var p4 = null;
+var p5 = null;
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("content", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("content");
+  ev.target.appendChild(document.getElementById(data));
+
+  //Keeps track of the answer places:
+  //Ex - currentTarget.id = div6, data = answer1
+  switch(ev.currentTarget.id) {
+    case 'div6':
+      p1 = data;
+      break;
+    case 'div7':
+      p2 = data;
+      break;
+    case 'div8':
+      p3 = data;
+      break;
+    case 'div9':
+      p4 = data;
+      break;
+    case 'div10':
+      p5 = data;
+      break;
+    default:
+      alert("Try Again");
+  }
+}
+
+function check(){
+
+  //Highlight the correct answers in green
+  if(p1 == "answer1") {
+    document.getElementById('div6').style.borderColor = "green";
+  }
+  if(p2 == "answer2") {
+    document.getElementById('div7').style.borderColor = "green";
+  }
+  if(p3 == "answer3") {
+    document.getElementById('div8').style.borderColor = "green";
+  }
+  if(p4 == "answer4") {
+    document.getElementById('div9').style.borderColor = "green";
+  }
+  if(p5 == "answer5") {
+    document.getElementById('div10').style.borderColor = "green";
+  }
+
+  //Check if all the answers are correct
+  if(p1 == "answer1" && p2 == "answer2" && p3 == "answer3" &&
+     p4 == "answer4" && p5 == "answer5") {
+
+    alert("Congrats Space Cadet! You may continue on your space adventure!");
+  }
+
+}
