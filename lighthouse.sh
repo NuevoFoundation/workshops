b9826f22ebf621f503a36b4d59d58deca21ef7ad
Jackie Elliott <jaellio@microsoft.com>
2020-07-29 12:09:14 -0700

    Limit number of runs to 1

--- lighthouse.sh
+++ lighthouse.sh
@@ -3,7 +3,7 @@ trace_dir() {
     for pathname in "$1"/*; do 
         if [ -d "$pathname" ]; then
             echo "$pathname"
-            lhci autorun --collect.static-dist-dir="$pathname"
+            lhci autorun --collect.static-dist-dir="$pathname" --collect.number-of-runs=1
         fi
     done                
 }
