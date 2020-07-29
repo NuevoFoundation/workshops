e15f535944c35ce9b7515f13ff7d3e076a2652d3
Jackie Elliott <jaellio@microsoft.com>
2020-07-29 12:04:14 -0700

    Fixed typo

--- lighthouse.sh
+++ lighthouse.sh
@@ -3,7 +3,7 @@ trace_dir() {
     for pathname in "$1"/*; do 
         if [ -d "$pathname" ]; then
             echo "$pathname"
-            lhci autorun --collect.static-dist-dir="$(dirname $pathname)" --collect.url="$pathname"
+            lhci autorun --collect.static-dist-dir="$pathname"
         fi
     done                
 }
