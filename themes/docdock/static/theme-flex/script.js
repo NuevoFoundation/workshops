5558d1451a4bf69ad2628f3f9816153edfe54c9c
Wendy Breiding (She/Her) <webreidi@microsoft.com>
2026-09-15 14:07:22 -0700

    Enhance language selector functionality to navigate on change with URL validation

--- themes/docdock/static/theme-flex/script.js
+++ themes/docdock/static/theme-flex/script.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function () {
+  jQuery(".language-selector").on("change", function () {
+    var destination = new URL(this.value, window.location.href);
+    var isHttp = destination.protocol === "http:" || destination.protocol === "https:";
+
+    if (isHttp && destination.origin === window.location.origin) {
+      window.location.assign(destination.href);
+    }
+  });
+
   jQuery(".skip-link").on("click", function (event) {
     var target = document.querySelector(this.hash);
     if (!target) {
